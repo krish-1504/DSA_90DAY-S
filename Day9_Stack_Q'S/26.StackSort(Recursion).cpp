00b98df5367102930086533f10ca730817b68d2d
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-11 18:28:22 +0530

    Create 26.StackSort(Recursion).cpp

---
+++ Day9_Stack_Q'S/26.StackSort(Recursion).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+void sortit(stack<int> &st ,int num){
+	if(st.empty() || num > st.top()){
+		st.push(num);
+		return;
+	}
+	int x = st.top();
+	st.pop();
+	sortit(st,num);
+	st.push(x);
+}
+void solve(stack<int> &st){
+	if(st.empty()){
+		return ;
+	}
+	int y = st.top();
+	st.pop();
+	solve(st);
+	sortit(st,y);
+
+}
+void sortStack(stack<int> &st)
+{
+	solve(st);
+}
+
